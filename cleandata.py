02d24b75ba22d95089c89357b103c0c1cdc9684c
Charley Maldonado <73445972+CharleyB3000@users.noreply.github.com>
2026-06-23 20:03:36 -0600

    clean dt
    file of clean data

---
+++ cleandata.py
@@ -0,0 +1,10 @@
+
+#Import and configure the required modules.
+# Define required imports
+import pandas as pd
+import numpy as np
+import sys
+import re
+# These set pandas max column and row display in the notebook
+pd.set_option('display.max_columns', 50)
+pd.set_option('display.max_rows', 50)
